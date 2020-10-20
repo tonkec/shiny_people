1416f9dc97b158ca2f0dfba74b28d7b87ffb35b3
Antonija Simic <antonija1023@gmail.com>
2020-10-20 12:04:14 +0200

    add custom hook to handle input change

--- src/Form.js
+++ src/Form.js
@@ -1,29 +1,52 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { PeopleContext } from "./context";
+const useForm = (initialValues) => {
+  const [values, setValues] = useState(initialValues);
+
+  return {
+    values,
+    handleChange: (e) => {
+      setValues({
+        ...values,
+        [e.target.name]: e.target.value,
+      });
+    },
+    reset: () => setValues(initialValues),
+  };
+};
 
 const Form = ({ id, isEdit }) => {
-  const [name, setName] = React.useState("");
+  const { values, handleChange, reset } = useForm({
+    name: "",
+  });
+
   const { people, dispatch } = useContext(PeopleContext);
-  const onChange = (e) => {
-    setName(e.target.value);
-  };
 
   const setAction = () => (isEdit ? startEditPerson() : startAddPerson());
 
-  const startEditPerson = () =>
+  const startEditPerson = () => {
     dispatch({ type: "EDIT_PERSON", people: { name: "smth new here" } });
+  };
 
   const startAddPerson = () =>
-    dispatch({ type: "ADD_PERSON", people: { name } });
+    dispatch({ type: "ADD_PERSON", people: { name: values.name } });
 
   const onSubmit = (e) => {
     e.preventDefault();
+    reset();
     setAction();
   };
 
   return (
     <form onSubmit={onSubmit}>
-      <input type="text" value={name} onChange={onChange} />
+      <input
+        type="text"
+        type="text"
+        name="name"
+        placeholder="Enter your name..."
+        onChange={handleChange}
+        value={values.name}
+      />
       <input type="submit" />
     </form>
   );
