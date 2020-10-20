45e57a3ff5343ead76b70b85c808e25d271f242b
Antonija Simic <antonija1023@gmail.com>
2020-10-20 19:06:06 +0200

    fix typo in the name of component

--- src/Routes.js
+++ src/Routes.js
@@ -2,11 +2,11 @@ import React from "react";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
 import CreatePersonPage from "./CreatePersonPage";
 import EditPersonPage from "./EditPersonPage";
-import Homepage from "./Homepage";
+import HomePage from "./HomePage";
 const Routes = () => (
   <BrowserRouter>
     <Switch>
-      <Route path="/" component={Homepage} exact={true}></Route>
+      <Route path="/" component={HomePage} exact={true}></Route>
       <Route path="/create" component={CreatePersonPage}></Route>
       <Route path="/edit/:id" component={EditPersonPage}></Route>
     </Switch>
