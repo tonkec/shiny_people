4517c9cc4c65beabe2eba0bb1ea4eb5a56498f73
Antonija Simic <antonija1023@gmail.com>
2020-10-21 17:12:12 +0200

    add more countires

--- src/components/Form/countries.js
+++ src/components/Form/countries.js
@@ -1,6 +1,9 @@
 const options = [
   { value: "Austria", label: "Austria" },
   { value: "Croatia", label: "Croatia" },
+  { value: "Portugal", label: "Portugal" },
+  { value: "Germany", label: "Germany" },
+  { value: "India", label: "India" },
   { value: "Peru", label: "Peru" },
 ];
 
