9c8b71d6d32bce381aa05c14597589492a94cd99
Antonija Simic <antonija1023@gmail.com>
2020-10-21 18:01:17 +0200

    set default user

--- src/context/context.js
+++ src/context/context.js
@@ -1,11 +1,24 @@
 import React, { useReducer, useEffect } from "react";
 import { reducer } from "./reducer";
+import { v4 as uuidv4 } from "uuid";
+
 export const PeopleContext = React.createContext();
+
+const defaultUser = {
+  name: "Derek Trotter",
+  title: "Independent Trader",
+  country: "United Kingdom",
+  salary: "3000",
+  birth: "1/1/1960",
+  id: uuidv4(),
+};
 
 export const PeopleContextProvider = (props) => {
   const [people, dispatch] = useReducer(reducer, [], () => {
     const dataFromLocalStorage = localStorage.getItem("people");
-    return dataFromLocalStorage ? JSON.parse(dataFromLocalStorage) : [];
+    return dataFromLocalStorage
+      ? JSON.parse(dataFromLocalStorage)
+      : [defaultUser];
   });
 
   useEffect(() => {
