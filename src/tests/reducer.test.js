76b76aaf399e29e71c8f5329e8b78e5992dcd738
Antonija Simic <antonija1023@gmail.com>
2020-10-23 12:16:04 +0200

    test ADD_PERSON action

---
+++ src/tests/reducer.test.js
@@ -0,0 +1,22 @@
+import { reducer, ADD_PERSON, EDIT_PERSON, defaultUser } from "context/reducer";
+import expect from "expect";
+import people from "./fixtures/people";
+
+describe("people reducer", () => {
+  it("should add new person", () => {
+    const person = {
+      name: "New Personr",
+      title: "New Title",
+      country: "United Kingdom",
+      salary: "35000",
+      birth: "1940-01-01",
+      id: expect.any(String),
+    };
+    const action = {
+      type: ADD_PERSON,
+      person,
+    };
+    const state = reducer(people, action);
+    expect(state).toEqual([...people, person]);
+  });
+});
