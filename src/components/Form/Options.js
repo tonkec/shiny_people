ad94860f31cf80d587b0744d0878d2872f338626
Antonija Simic <antonija1023@gmail.com>
2020-10-23 10:26:37 +0200

    remove unnecessary check for empty object

--- src/components/Form/Options.js
+++ src/components/Form/Options.js
@@ -3,14 +3,12 @@ import countries from "./countries";
 
 const Options = ({ currentPerson }) => {
   const defaultValue = countries[0].label;
-  const isEmpty =
-    Object.keys(currentPerson).length === 0 &&
-    currentPerson.constructor === Object;
+
   const preselectedOption = (
     <option>{currentPerson.country || defaultValue}</option>
   );
   const optionCountries = countries.map((country, key) => {
-    if (!isEmpty) {
+    if (currentPerson.country !== undefined) {
       if (currentPerson.country !== country.label) {
         return (
           <option key={key} value={country.label}>
