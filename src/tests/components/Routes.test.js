00a4ea17ceeb89659b7dc764499ea89c6d725243
Antonija Simic <antonija1023@gmail.com>
2020-10-25 10:52:02 +0100

    test routes

---
+++ src/tests/components/Routes.test.js
@@ -0,0 +1,36 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { createMemoryHistory } from "history";
+import React from "react";
+import { Router } from "react-router-dom";
+import "@testing-library/jest-dom/extend-expect";
+import App from "../../App";
+
+describe("<App />", () => {
+  beforeEach(() => {
+    const history = createMemoryHistory();
+    render(
+      <Router history={history}>
+        <App />
+      </Router>
+    );
+  });
+
+  it("shows edit page when clicked on an edit button", () => {
+    const leftClick = { button: 0 };
+    userEvent.click(screen.getByText(/Edit/i), leftClick);
+    expect(screen.getByText(/Edit employee/i)).toBeInTheDocument();
+  });
+
+  it("shows home page when clicked on the cancel button", () => {
+    const leftClick = { button: 0 };
+    userEvent.click(screen.getByText(/Cancel/i), leftClick);
+    expect(screen.getByText(/People/i)).toBeInTheDocument();
+  });
+
+  it("shows create page when clicked on the Add Employee button", () => {
+    const leftClick = { button: 0 };
+    userEvent.click(screen.getByText(/Add Employee/i), leftClick);
+    expect(screen.getByText(/Add a new employee/i)).toBeInTheDocument();
+  });
+});
